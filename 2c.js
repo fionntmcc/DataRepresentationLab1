77f8d43799b1e8179351886d33e694244ff63dee
Fionn McCarthy <g00414386@atu.ie>
2024-09-27 10:41:10 +0000

    2c) Completed

---
+++ 2c.js
@@ -0,0 +1,11 @@
+/*
+    Fionn McCarthy
+    G00414386@atu.ie
+*/
+
+// arrow function adds var 1 and var 2 together
+let c = (num1, num2) => {
+    console.log(num1 + " + " + num2 + " = " + (num1 + num2));
+}
+
+c(5, 6);
