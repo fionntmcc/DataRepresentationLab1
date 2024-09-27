f460d5d4d5f1f43cc1fa88be371a829d1397d639
Fionn McCarthy <g00414386@atu.ie>
2024-09-27 10:33:44 +0000

    2b) completed - print out variable

---
+++ 2b.js
@@ -0,0 +1,11 @@
+/*
+    Fionn McCarthy
+    G00414386@atu.ie
+*/
+
+// arrow function prints out log of variable's toString()
+let b = (value)=> {
+    console.log(value.toString());
+}
+
+b("variable");
