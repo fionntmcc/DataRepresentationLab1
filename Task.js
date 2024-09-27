7f44120d7dbd1909f74c0c3d6d053adfff1c1fcc
Fionn McCarthy <g00414386@atu.ie>
2024-09-27 11:09:28 +0000

    2b) addTask() created

--- Task.js
+++ Task.js
@@ -1,2 +1,18 @@
-let stringArr = ["Fionn", "Paul", "Mark", "Joseph"];
+/*
+    Fionn McCarthy
+    G00414386@atu.ie
+*/
 
+// instance array of strings
+const stringArr = ["Fionn", "Paul", "Mark", "Joseph"];
+
+function addTask(task) {
+    // push task onto array
+    stringArr.push(task);
+
+    // print out array
+    console.log(task + " added to string arr.");
+    console.log("New array: " + stringArr);
+}
+
+addTask("Pauleen");
