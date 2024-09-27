7a3b2149674c8e9eaf184521f6fe8d64094844d6
Fionn McCarthy <g00414386@atu.ie>
2024-09-27 10:29:45 +0000

    2a) Completed - arrow function

---
+++ 2a.js
@@ -0,0 +1,20 @@
+/*
+    Fionn McCarthy
+    G00414386@atu.ie
+*/
+
+// example of functional programming
+// this is an arrow function
+let a = ()=> {
+    console.log("Data Representation & Data Querying");
+}
+
+a();
+
+/*
+let b = (value)=> {
+    console.log(value);
+}
+
+b("wowee");
+*/
